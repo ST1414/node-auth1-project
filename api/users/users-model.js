204a910d984742153953c9c9c6ce2befd3094756
ST1414 <sterry1414@gmail.com>
2021-12-13 14:16:26 -0800

    Users-model completed

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -10,15 +10,18 @@ function find() {
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
-function findBy(filter) {
-  return db('users').select('user_id', 'username').where(filter);
+function findBy(filter) { // PASSING FILTER AN OBJECT
+  return db('users').select('user_id', 'username').where(filter); 
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
-function findById(user_id) {
-  return db('users').select('user_id', 'username').where(user_id);
+function findById(user_id) { // PASSING AN INTEGER
+  return db('users').select('user_id', 'username').where('user_id', user_id).first(); // This constructs an object in the where clause
+  // return db('users').select('user_id', 'username').where( { user_id } ).first(); // OR could pass user_id this way)
+  // return db('users').select('user_id', 'username').where( { user_id: `${user_id}` } ).first(); // OR could pass user_id this way, where we pass the variable user_id as a string
+
 }
 
 /**
